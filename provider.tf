91b426b80c6f8bb7e5fa157f27da57eee4ba27c9
ijju <82495873+ijju0437@users.noreply.github.com>
2021-07-02 11:52:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,8 @@ provider "aws" {
  secret_key = "j+k7ehBPgX+zOj7JYr4PONoHSQzcl13hJHwHXm1z"
  region = "us-east-1"
 }
+
+resource "aws_instance" "myFirstInstance" {
+ ami = "ami-0747bdcabd34c712a"
+ instance_type = "t2.micro"
+}
